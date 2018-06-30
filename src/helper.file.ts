56b77f4bb2ce98d899663c729d62c27c56febc82
SibeeshVenu <sibikv4u@gmail.com>
2018-06-30 11:15:14 +0530

    adding deleted code

--- src/helper.file.ts
+++ src/helper.file.ts
@@ -3,5 +3,9 @@ import { Buffer } from "buffer";
 
 export function readFile(path: string) {
     const fileData = fs.readFileSync(path).toString("hex");
-    return Buffer.from(fileData);
+    const result = [];
+    for (let index = 0; index < fileData.length; index += 2) {
+        result.push(parseInt(fileData[index] + "" + fileData[index + 1], 16));
+    }
+    return Buffer.from(result);
 }
